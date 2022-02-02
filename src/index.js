54dcaf37175b63bec318ce97187563a3c90f2714
Nich87 <touru10286@gmail.com>
2022-02-02 22:53:40 +0900

    Add Materialize Framework
    - Add Framework and test.
    - Add Electron-builder
    - delete components

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, dialog } = require('electron');
+const storage = require('electron-json-storage');
 const path = require('path');
 const config = require('./config.json');
 
 if (require('electron-squirrel-startup')) app.quit();
 
+let mainWindow;
+
 const createWindow = () => {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
   });
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
   if (config.debug) mainWindow.webContents.openDevTools();
 };
 
-app.on('ready', createWindow);
+
+const openFolder = {
+  label: 'Open Folder',
+  subMenu: [{
+    label: 'Folder',
+    id: 'folder',
+    click: function () {
+      openFolderDialog();
+    }
+  }]
+};
+
+app.on('ready', () =>{
+  createWindow();
+  SetMenu(openFolder);
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
@@ -22,3 +43,27 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
+
+function SetMenu(openFolder) {
+  const menu = Menu.buildFromTemplate([openFolder]);
+  Menu.setApplicationMenu(menu);
+}
+
+
+function openFolderDialog() {
+  dialog.showOpenDialog(mainWindow, { properties: ['openDirectory'] }).then(
+    (result) => {
+      const filePath = result.filePaths[0];
+      if (filePath) {
+        storage.set('path', { path: filePath }, function (error) {
+          if (error) throw error;
+        });
+        console.log(filePath);
+      }
+    },
+    (error) => {
+      throw error;
+    }
+  );
+}
+
