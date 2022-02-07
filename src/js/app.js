6270bc324c00718b9284f4f6ec6511c659595739
Nich87 <touru10286@gmail.com>
2022-02-07 16:30:21 +0900

    update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,59 @@
-let player;
+let player, list, isLoading = false;
 M.AutoInit();
 
-ipcRenderer.on('start', (args,filelist) => {
+ipcRenderer.on('start', (args, filelist) => {
     filelist = Object.values(filelist);
-    console.log(filelist)
-    Maker(filelist);
+    console.log(filelist);
+    list = filelist;
+    if (isLoading) {
+        player.unload();
+        isLoading = false;
+    }
+    Maker(list);
 });
 
 function Maker(list) {
     player = new Howl({
         src: list[0],
-        loop: true
+        loop: true,
+        autoplay: true
     });
-    player.on('load', () => player.play());
     player.on('end', () => {
         console.debug('end');
         player.unload();
-        list.shift();
+        list.push(list.shift());
         Maker(list);
     });
-    player.on('play', () => play());
+    player.on('play', () => play(player));
+    isLoading = true;
 }
 
-const play = () => {
+function play(player) {
+    const player_bt = document.getElementById('btn_play');
+    const skip = document.getElementById('btn_skip');
+    const previous = document.getElementById('btn_previous');
+    player_bt.addEventListener('click', (event) => {
+        if (player.playing()) {
+            event.target.innerText = 'play_arrow';
+            player.pause();
+        }
+        else {
+            event.target.innerText = 'pause';
+            player.play();
+        }
+    });
 
+    skip.addEventListener('click', () => {
+        list.push(list.shift());
+        player.unload();
+        Maker(list);
+        console.log(list);
+    });
+
+    previous.addEventListener('click', () => {
+        list.unshift(list.pop());
+        player.unload();
+        Maker(list);
+        console.log(list)
+    });
 }
